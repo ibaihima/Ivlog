03d0bb1ec176728b12a3b9de184dc992fd9aff89
Ibaihima <ibaihima@hotmail.com>
2022-08-23 01:13:36 -0400

    update

--- Ivlog/client/src/Card.js
+++ Ivlog/client/src/Card.js
@@ -3,7 +3,7 @@ import Comment from './Comment';
 import './Card.css'
 import {useState, useEffect} from 'react'
 
-function Card({user, image, text, button}){
+function Card({user, image, text, button, deletePost}){
     const [card, savedCards] = useState([])
     const [favorites, savedFavorites] = useState([])
 
@@ -48,7 +48,7 @@ function Card({user, image, text, button}){
         <div className="card"> 
             <div className="card-header"> 
                 <h1>{user.firstName} {user.lastName} </h1>
-                <button className="profilebttn">{button}</button>
+                <button onClick={(e) => deletePost(e)} className="profilebttn">{button}</button>
             </div>
             <div>
             <h1>@{user.username}</h1>
